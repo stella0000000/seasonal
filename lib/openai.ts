b38b1b0de97e7bb86426156be81515ac554ca985
stella <stella.choi@hyperisland.se>
2024-03-03 03:18:17 -0500

    rs

--- lib/openai.ts
+++ lib/openai.ts
@@ -1,7 +1,7 @@
 import openai from "openai";
 
 export const openaiInstance = new openai({
-  apiKey: process.env["NEXT_PUBLIC_OPENAI_API_KEY"], // This is the default and can be omitted
+  apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
 });
 
 export { openaiInstance as openai };
